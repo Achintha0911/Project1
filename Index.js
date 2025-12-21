6504745ac46425424829d5ef2e48fc5806ae5c56
Achintha0911 <en108908@foe.sjp.ac.lk>
2025-12-21 23:47:45 +0530

    Add basic API endpoint

--- Index.js
+++ Index.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Backend running");
+app.get("/api/message", (req, res) => {
+    res.json({
+        message: "Hello from backend" // Json object
+    });
 });
 
+const port = 3000;
 app.listen(3000, () => {
-  console.log("Server started");
+    console.log('Server started on port ${PORT} '); //changed port to use environment variable
 });
