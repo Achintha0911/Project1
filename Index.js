2b38e2d65719a54715cf6b850f3b8a30dec3afac
Achintha0911 <en108908@foe.sjp.ac.lk>
2025-12-25 23:23:47 +0530

    ADD POST endpoint to recieve messages

--- Index.js
+++ Index.js
@@ -1,13 +1,12 @@
 const express = require("express");
 const app = express();
 
-
 app.use(express.json());
 
 const messageRoutes = require("./Routes/messages.routes");
 app.use("/api", messageRoutes);
 
 const port = 3000;
-app.listen(3000, () => {
-    console.log('Server started on port ${PORT} '); //changed port to use environment variable
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
 });
--- Routes/messages.routes.js
+++ Routes/messages.routes.js
@@ -1,8 +1,19 @@
 const express = require("express");
 const router = express.Router();
+
 router.get("/message", (req, res) => {
     res.json({
-        message: "Hello from backend" // Json object
+        message: "Hello from backend"
     });
 });
+
+router.post("/message", (req, res) => {
+    const userMessage = req.body.message;
+    
+    res.json({
+        status: "received",
+        message: userMessage
+    });
+});
+
 module.exports = router;
